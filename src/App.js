76feee297b6b9ed90942a3645e506fd975a5aa73
Michaeldremy <michaeldremy@gmail.com>
2020-10-05 23:28:21 -0700

    Pure Redux Testing

--- src/App.js
+++ src/App.js
@@ -6,9 +6,29 @@ import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 // redux stuff
 
-
 // store - stores data, think of state
 // reducer - function that is used to update the store
+// two arugements - state, action
+// state - old state/state before update
+// action - what happened/what update
+// return updated or old state
+
+import { createStore } from "redux";
+
+// store.getState() - 
+
+// initial store
+const initialStore = {
+  count: 0,
+}
+// reducer
+const reducer = (state, action) => {
+  console.log({ state, action });
+  return state;
+};
+// store
+const store = createStore(reducer, initialStore);
+console.log(store.getState());
 
 function App() {
   // cart setup
